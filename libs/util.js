6cc724552c8f6edf612ec3af1a97a298de3448a4
郑思慧 <skye.zheng@dbappsecurity.com.cn>
2023-08-21 18:29:01 +0800

    添加名称验证规则

--- libs/util.js
+++ libs/util.js
@@ -3,3 +3,12 @@ import { isEqual } from 'lodash';
 export const judgeIsEqual = (newVal, oldVal) => {
   return isEqual(newVal, oldVal);
 }
+
+export const formatNumber = number => {
+  return number.toLocaleString()
+}
+
+export const verifyNumber = string => {
+  const regx = /[0-9]/
+  return regx.test(string);
+}
