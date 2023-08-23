4902972c6eb2c12816b86295865ba978bfbe7ace
郑思慧 <skye.zheng@dbappsecurity.com.cn>
2023-08-23 14:42:47 +0800

    修改内容

---
+++ common/components/input.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Input>submodule_child Input</Input>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
